9a57fcd3689635cd81c9445cde76a3fcfbc2cf91
yutaishikawa526 <yutaishikawa526@gmail.com>
2024-01-19 14:11:06 +0900

    iso形式のqemu起動シェルの軽微な修正

--- iso/iso_CentOS6.4/initial_install.sh
+++ iso/iso_CentOS6.4/initial_install.sh
@@ -1,12 +1,14 @@
-#!/bin/bash
+#!/bin/bash -e
 
 # 初回時
 # isoダウンロードからディスクイメージ作成、起動まで
 
-_ISO_PATH=`pwd`/disk_iso/CentOS.iso
-_QCOW2_PATH=`pwd`/disk_iso/CentOS.qcow2
+_EXCE_DIR=$(cd $(dirname $0) ; pwd)
+
+_ISO_PATH="$_EXCE_DIR"/disk_iso/CentOS.iso
+_QCOW2_PATH="$_EXCE_DIR"/disk_iso/CentOS.qcow2
 _ISO_URL='https://ftp.jaist.ac.jp/pub/Linux/CentOS-vault/6.4/isos/x86_64/CentOS-6.4-x86_64-minimal.iso'
 
-../common.sh
+source "$_EXCE_DIR"/../common.sh
 
 initial_install "$_ISO_PATH" "$_QCOW2_PATH" "$_ISO_URL"
