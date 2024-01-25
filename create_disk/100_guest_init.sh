a03c6b12058b95a853601b3455ed35f379b7e2ce
yutaishikawa526 <yutaishikawa526@gmail.com>
2024-01-26 02:51:23 +0900

    軽微な修正

--- create_disk/100_guest_init.sh
+++ create_disk/100_guest_init.sh
@@ -8,11 +8,10 @@ echo '候補'
 ip a \
     | grep -E '^[1-9][0-9]*: [a-zA-Z0-9]+: ' \
     | sed -r 's#^[1-9][0-9]*: ([a-zA-Z0-9]+): .*$#\1#g'
-read -p net_name
+read -p ":" net_name
 sed -i 's#^Name=.*$#Name='$net_name'#g' '/etc/systemd/network/ethernet.network'
 
 systemctl restart systemd-networkd
 
 # grubの更新
-grub-install
 update-grub
