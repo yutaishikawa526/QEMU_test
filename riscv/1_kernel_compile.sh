85337bb9b85a97c73d2f4a9359d2c457ef698e34
yutaishikawa526 <yutaishikawa526@gmail.com>
2024-02-02 19:26:56 +0900

    openSBIのコンパイル時にubootをpayloadするように修正

--- riscv/1_kernel_compile.sh
+++ riscv/1_kernel_compile.sh
@@ -8,8 +8,12 @@ export_env "$_DIR"
 
 is_dir "$_LINUX_SRC"
 
-(cd "$_LINUX_SRC";make ARCH=riscv CROSS_COMPILE=riscv64-linux-gnu- defconfig)
-(cd "$_LINUX_SRC";make ARCH=riscv CROSS_COMPILE=riscv64-linux-gnu- menuconfig)
-(cd "$_LINUX_SRC";make ARCH=riscv CROSS_COMPILE=riscv64-linux-gnu- -j $(nproc))
+cd "$_LINUX_SRC"
+
+make ARCH=riscv CROSS_COMPILE=riscv64-linux-gnu- defconfig
+make ARCH=riscv CROSS_COMPILE=riscv64-linux-gnu- menuconfig
+make ARCH=riscv CROSS_COMPILE=riscv64-linux-gnu- -j $(nproc)
 
 sudo cp "$_LINUX_SRC/arch/riscv/boot/Image" "$_KERNEL_PATH"
+
+cd "$_DIR"
--- riscv/3_opensbi_compile.sh
+++ riscv/3_opensbi_compile.sh
@@ -1,13 +1,28 @@
 #!/bin/bash -ex
 
-# openSBIのコンパイル
+# uboot付きのopenSBIのコンパイル
 
 _DIR=$(cd $(dirname $0) ; pwd)
 source "$_DIR/com/com.sh"
 export_env "$_DIR"
 
 is_dir "$_OPENSBI_SRC"
+is_dir "$_UBOOT_SRC"
 
-(cd "$_OPENSBI_SRC";make PLATFORM=generic ARCH=riscv CROSS_COMPILE=riscv64-linux-gnu- -j $(nproc))
+# ubootのコンパイル
+cd "$_UBOOT_SRC"
+make ARCH=riscv CROSS_COMPILE=riscv64-linux-gnu- defconfig
+make ARCH=riscv CROSS_COMPILE=riscv64-linux-gnu- -j $(nproc)
 
-sudo cp "$_OPENSBI_SRC/build/platform/generic/firmware/fw_jump.elf" "$_OPENSBI_PATH"
+sudo cp "$_UBOOT_SRC/u-boot" "$_UBOOT_PATH"
+
+# openSBIのコンパイル
+# [https://github.com/riscv-software-src/opensbi/blob/v1.3/docs/platform/qemu_virt.md]
+cd "$_OPENSBI_SRC"
+make PLATFORM=generic ARCH=riscv \
+    FW_PAYLOAD_PATH="$_UBOOT_PATH" \
+    CROSS_COMPILE=riscv64-linux-gnu- -j $(nproc)
+
+sudo cp "$_OPENSBI_SRC/build/platform/generic/firmware/fw_payload.elf" "$_OPENSBI_PATH"
+
+cd "$_DIR"
