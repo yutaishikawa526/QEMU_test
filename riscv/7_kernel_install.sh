577e7cfa278c22cca51a586f72c0b7616612d1f5
y_ishikawa <yutaishikawabg@gmail.com>
2024-02-02 10:47:47 +0900

    軽微な修正

--- riscv/7_kernel_install.sh
+++ riscv/7_kernel_install.sh
@@ -18,7 +18,6 @@ loopback=`set_device "$_DISK_PATH"`
 # rootパーティションのデバイスを取得
 root_partid=`name_to_partid "$_DISK_PATH" 'root'`
 root_dev=`sudo findfs PARTUUID="$root_partid"`
-root_uuid=`get_uuid_by_device "$root_dev"`
 
 # マウント
 tmp_mnt="$_DIR/disk/tmp_mnt"
@@ -28,7 +27,7 @@ sudo mount "$root_dev" "$tmp_mnt"
 sudo cp "$_DIR/x2_kernel_install.sh" "$tmp_mnt/root/x2_kernel_install.sh"
 sudo chmod +x "$tmp_mnt/root/x2_kernel_install.sh"
 
-sleep 3
+sleep 1
 
 # アンマウント
 umount_all "$tmp_mnt"
@@ -36,13 +35,16 @@ umount_all "$tmp_mnt"
 # ディスクの解除
 unset_device "$_DISK_PATH"
 
+sleep 1
+
 # ---- 起動 ----
 
 # 起動
+# !!!注:rootをUUIDによる指定ができない
 sudo qemu-system-riscv64 -machine virt -m 2048 \
     -kernel "$_KERNEL_PATH" \
     -bios "$_OPENSBI_PATH" \
-    -append "root=UUID=$root_uuid rw console=ttyS0" \
+    -append "root=/dev/vda4 rw console=ttyS0" \
     -drive file="$_DISK_PATH",format=raw,media=disk,id=hd1 \
     -device virtio-blk-device,drive=hd1 \
     -netdev user,id=net0 -device virtio-net-device,netdev=net0
