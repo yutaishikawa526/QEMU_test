d957544a7981de769a4fd1acf36258577519c11f
yutaishikawa526 <yutaishikawa526@gmail.com>
2024-02-04 02:02:05 +0900

    軽微な修正

--- riscv/3_opensbi_compile.sh
+++ riscv/3_opensbi_compile.sh
@@ -14,10 +14,7 @@ cd "$_UBOOT_SRC"
 make ARCH=riscv CROSS_COMPILE=riscv64-linux-gnu- qemu-riscv64_smode_defconfig
 make ARCH=riscv CROSS_COMPILE=riscv64-linux-gnu- -j $(nproc)
 
-# openSBIのコンパイル
-cd "$_OPENSBI_SRC"
-make PLATFORM=generic ARCH=riscv CROSS_COMPILE=riscv64-linux-gnu- -j $(nproc)
-sudo cp "$_OPENSBI_SRC/build/platform/generic/firmware/fw_jump.elf" "$_OPENSBI_PATH"
+sleep 3
 
 # uboot付き
 # [https://github.com/riscv-software-src/opensbi/blob/v1.3/docs/platform/qemu_virt.md]
@@ -25,6 +22,8 @@ cd "$_OPENSBI_SRC"
 make PLATFORM=generic ARCH=riscv \
     FW_PAYLOAD_PATH="$_UBOOT_SRC/u-boot.bin" \
     CROSS_COMPILE=riscv64-linux-gnu- -j $(nproc)
+
+sudo cp "$_OPENSBI_SRC/build/platform/generic/firmware/fw_jump.elf" "$_OPENSBI_PATH"
 
 sudo cp "$_OPENSBI_SRC/build/platform/generic/firmware/fw_payload.elf" "$_OPENSBI_UBOOT_PATH"
 
